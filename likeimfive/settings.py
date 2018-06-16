b403dddb7e39323fe2622e2fcf4b6e01bd1a5a9f
Cameron Braverman <bravermancj@gmail.com>
2018-06-16 17:50:56 -0400

    cool

--- likeimfive/settings.py
+++ likeimfive/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'post.apps.PostConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'likeimfive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'likeimfive',
+        'USER': 'cameron',
+        'PASSWORD': 'koolcam3198',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- post/models.py
+++ post/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    date_joined = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateField(default=datetime.date.today)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return
